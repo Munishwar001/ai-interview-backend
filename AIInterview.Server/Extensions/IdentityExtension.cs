828602e903bcac70b721dc051d4ac40ce237b5f7
Munishwar kalra <kalramunishwar.com>
2026-04-16 10:43:08 +0530

    updated the identity passwor validation

--- AIInterview.Server/Extensions/IdentityExtension.cs
+++ AIInterview.Server/Extensions/IdentityExtension.cs
@@ -8,6 +8,16 @@ namespace AIInterview.Server.Extensions
     {
         public static IServiceCollection ConfigureIdentity(this IServiceCollection services)
         {
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password validation - minimum 6 characters only
+                options.Password.RequiredLength = 6;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+            });
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
